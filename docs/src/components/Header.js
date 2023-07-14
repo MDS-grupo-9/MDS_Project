bd7b687aa898b785f6bee37f12d899f777d13bf8
JoelSRangel <joelsoaresrngl@gmail.com>
2023-07-14 00:05:54 -0300

    :repeat: (#): Arrumando o deploy no github pages

--- docs/src/components/Header.js
+++ docs/src/components/Header.js
@@ -7,17 +7,17 @@ function Header() {
     return(
         <header>
             <div className="logo">
-                <Link onClick={() => window.location.assign("/#body")}>
+                <Link onClick={() => window.location.assign("/MDS_Project/#body")}>
                     <img src={unb_logo} alt="logo UnB" class="unb_logo" width={150}/>
                     <img src={logo} alt="logo observatório de energia" width={45}/>
                 </Link>
             </div>
             <nav>
                 <ul>
-                    <li><Link onClick={() => window.location.assign("/#body")}>Página Inicial</Link></li>
-                    <li><Link onClick={() => window.location.assign("/#quem_somos")}>Quem somos</Link></li>
-                    <li><Link to="/bancoDados">Banco de dados</Link></li>
-                    <li><Link onClick={() => window.location.assign("/#prof")}>Professores</Link></li>
+                    <li><Link onClick={() => window.location.assign("/MDS_Project/#body")}>Página Inicial</Link></li>
+                    <li><Link onClick={() => window.location.assign("/MDS_Project/#quem_somos")}>Quem somos</Link></li>
+                    <li><Link to="/MDS_Project/bancoDados">Banco de dados</Link></li>
+                    <li><Link onClick={() => window.location.assign("/MDS_Project/#prof")}>Professores</Link></li>
                 </ul>
             </nav>
         </header>
